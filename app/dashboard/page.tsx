2831edb8ccab120ca8e74ca1129bc281522a5756
Tanay Kumar <tanay25000@gmail.com>
2025-10-02 18:30:41 +0530

    cleaneduptemplate dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/lib/supabase/server";
+import { InfoIcon } from "lucide-react";
+
+export default async function ProtectedPage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getClaims();
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  }
+
+  return (
+
+    <h2 className="font-bold text-2xl mb-4">lorem ipsum</h2>
+
+  );
+}
